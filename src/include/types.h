c9ed0aace527c1484bdd8bf61e35637594f3708d
FoxLightning <bogdanlisichenko@gmail.com>
2024-07-25 16:48:34 +0200

    Updated types

--- src/include/types.h
+++ src/include/types.h
@@ -2,8 +2,9 @@
 
 #include <boost/geometry.hpp>
 #include <boost/geometry/geometries/point.hpp>
+#include <cstdint>
 
 typedef boost::geometry::model::d2::point_xy<double> Vector2D;
 typedef boost::geometry::model::d2::point_xy<float> Vector2F;
-typedef boost::geometry::model::d2::point_xy<int> Vector2I;
-typedef boost::geometry::model::d2::point_xy<long> Vector2L;
+typedef boost::geometry::model::d2::point_xy<int32_t> Vector2I;
+typedef boost::geometry::model::d2::point_xy<int64_t> Vector2L;
