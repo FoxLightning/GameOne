a27efd2ea646193968f53cf4380edb2debad1a4d
FoxLightning <bogdanlisichenko@gmail.com>
2024-08-03 10:25:17 +0200

    Update texture paths

--- src/cpp/Constants.cpp
+++ src/cpp/Constants.cpp
@@ -3,6 +3,6 @@
 namespace Const
 {
 const char *const ship = "resurces/textures/Ship_1_A_Small.png";
-const char *const enemy = "resurces/textures/Ship_1_B_Small.png";
-const char *const missle = "resurces/textures/Ship_1_C_Small.png";
+const char *const enemy = "resurces/textures/Enemy_1_A_Small.png";
+const char *const missle = "resurces/textures/Missile_A_Small.png";
 } // namespace Const
